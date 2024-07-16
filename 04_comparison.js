7774e46fdfc0c8578ccf815f3143bcf9e56bad8e
Ishanpathak1 <88367734+Ishanpathak1@users.noreply.github.com>
2024-07-16 10:55:28 +0530

    comparison

---
+++ 04_comparison.js
@@ -0,0 +1,12 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2 !=1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+//7,8 and 9 are issues in js, this is not the correct code
+
+// === this is strictly check, it will also check the data type, so "2" == 2 will come as true, but "2"===2 will be false
