d8c228f45b8e73044b2dbf987e82006dea85f975
Ishanpathak1 <88367734+Ishanpathak1@users.noreply.github.com>
2024-07-27 19:37:49 +0530

    While and Do While loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+let index=0
+
+while (index<=10) {
+    //console.log(`Value of index is ${index}`);
+    index=index+2;
+}
+
+let myArray =["Iron Man", "Spider Man", "Groot"]
+let Arr = 0
+while (Arr<myArray.length){
+    //console.log(`Value is ${myArray[Arr]}`)
+    Arr++;
+}
+
+//do while
+
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score ++
+} while (score<=10);
