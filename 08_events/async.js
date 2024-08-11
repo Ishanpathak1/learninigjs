617acbe90d021c30f3530918ad706e7c0d5e1868
Ishanpathak1 <88367734+Ishanpathak1@users.noreply.github.com>
2024-08-11 22:03:50 +0530

    Async Notes

---
+++ 08_events/async.js
@@ -0,0 +1,20 @@
+//Javascript is synchronous
+//It is a single threaded
+
+//Execution Context
+//Execute one line of code at a time
+//console.log-1
+//console.log-2
+//Each operation waits for the last one to complete befor executing
+
+//Blocking code vs Non-Blocking Code
+//Blocking code
+//Block the flow of the program
+//Read File Sync
+
+//Non Blocking Code
+//Does not block Execution
+//Read File Async
+
+
+// check the image at - https://youtu.be/zgt5oTD3rRc?si=VJ_MKvfH1OzehyY3 - timestamp - 10:19
