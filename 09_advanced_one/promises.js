e0824211808f614d8e73c7e3635649c77b41976e
Ishanpathak1 <88367734+Ishanpathak1@users.noreply.github.com>
2024-08-18 08:47:31 +0530

    FirstDayinUSA

---
+++ 09_advanced_one/promises.js
@@ -0,0 +1,26 @@
+//Most of the times we will be consuming the Promises
+const promiseOne = new Promise( function(resolve, reject){
+    //Do an async task
+    //Database calls
+    //Cryptography
+    //network calls
+    setTimeout(() => {
+        console.log('Async task is complete');
+        resolve()
+    }, 1000);
+})
+
+promiseOne.then(function () {
+    console.log("Promise Consumed");
+})
+
+new Promise( function (resolve, reject){
+    setTimeout(() => {
+        console.log("Async Task 2 ");
+        resolve()
+    }, 1000);
+}).then(function(){
+    console.log("Async 2 resolved");
+})
+
+//Video 40- 19:26
