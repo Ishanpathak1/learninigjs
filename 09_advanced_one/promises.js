84f3a95ff85f91cf3dc764328297d3baec4f214d
Ishanpathak1 <88367734+Ishanpathak1@users.noreply.github.com>
2024-08-23 09:36:36 -0400

    This video is completed, but I will do this whole thing again

--- 09_advanced_one/promises.js
+++ 09_advanced_one/promises.js
@@ -66,8 +66,32 @@ const promiseFive =new Promise(function(resolve,reject){
 });
 
 async function consumePromiseFive(){
-    const response = await promiseFive
-    console.log(response);
+    try{
+        const response = await promiseFive
+        console.log(response);
+    } catch (error){
+        console.log(error);
+    }
 }
 
 consumePromiseFive()
+
+async function getAllUsers(){
+    try{
+        const response= await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        console.log(data);
+
+    } catch {
+        console.log("E: ", error);
+
+    }
+}
+//getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+    return response.json()
+})
+.then((data)=>{console.log(data);})
+.catch((error)=> console.log(error))
