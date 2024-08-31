28ac361f26ef2b32b4d79310b35c2ce8b2fef793
Ishanpathak1 <88367734+Ishanpathak1@users.noreply.github.com>
2024-08-30 22:21:33 -0400

    Call and this in JS

---
+++ 10_classes_and_objects/call.js
@@ -0,0 +1,17 @@
+function setUsername(username) {
+    //complex db calls
+    this.username =username
+}
+
+function createUser(username, email , password) {
+    setUsername(username) //many think that this will give the access in this function too, but is not called here, so you have to do this
+    setUsername.call(this, username) // this is little bit tricky so please refer the video 44
+
+    this.email = email
+    this.password = password
+}
+
+const coffee= new createUser("coffee" , "coffee@google.com", "123")
+console.log(coffee);
+
+// refer video 44 this 
