b37f52ce483a03780d872a9dbc1df94a96e33a5d
Ishanpathak1 <88367734+Ishanpathak1@users.noreply.github.com>
2024-08-31 20:20:07 -0400

    No such message

--- 10_classes_and_objects/call.js
+++ 10_classes_and_objects/call.js
@@ -5,7 +5,7 @@ function setUsername(username) {
 
 function createUser(username, email , password) {
     setUsername(username) //many think that this will give the access in this function too, but is not called here, so you have to do this
-    setUsername.call(this, username) // this is little bit tricky so please refer the video 44
+    setUsername.call(this, username) // this is little bit tricky so please refer the video 44 
 
     this.email = email
     this.password = password
